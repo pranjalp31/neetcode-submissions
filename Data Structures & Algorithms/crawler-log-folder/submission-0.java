79f0d36a34ca4d7cf9fa4e89a33d5509eb383169
pranjalp <102456855+pranjalp31@users.noreply.github.com>
2026-07-20 23:23:42 +0530

    Add: crawler-log-folder - submission-0

---
+++ Data Structures & Algorithms/crawler-log-folder/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minOperations(String[] logs) {
+        int count=0;
+        for(String s:logs){
+            if(s.equals("./")){
+                continue;
+            }
+            if(s.equals("../")){
+                count=Math.max(0,count-1);
+            }
+            else{
+                count++;
+            }
+        }
+        return count;
+        
+    }
+}
